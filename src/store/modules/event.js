387b22cadef681ab857d49c67010a4dd619fcbe9
roytiger81102 <roytiger81102@gmail.com>
2020-04-29 17:54:59 +0900

    first commit

---
+++ src/store/modules/event.js
@@ -0,0 +1,134 @@
+import EventService from '@/services/EventService.js'
+
+export const namespaced = true
+
+export const state = {
+  events: [],
+  eventTotal: 0,
+  event: {}
+}
+export const mutations = {
+  ADD_EVENT(state, event) {
+    state.events.push(event)
+  },
+  SET_EVENTS(state, events) {
+    state.events = events
+  },
+  SET_EVENT_TOTAL(state, eventTotal) {
+    state.eventTotal = eventTotal
+  },
+  SET_EVENT(state, event) {
+    state.event = event
+  }
+}
+export const actions = {
+  createEvent({ commit }, event) {
+    return EventService.postEvent(event)
+      .then(() => {
+        commit('ADD_EVENT', event)
+      })
+      .catch(() => {
+        console.log('There was a problem creating your event')
+      })
+  },
+  fetchEvents({ commit }, { perPage, page }) {
+    EventService.getEvents(perPage, page)
+      .then(response => {
+        commit('SET_EVENTS', response.data)
+        commit('SET_EVENT_TOTAL', parseInt(response.headers['x-total-count']))
+      })
+      .catch(error => {
+        console.log('There was an error:', error.response)
+      })
+  },
+  fetchEvent({ commit, getters }, id) {
+    let event = getters.getEventById(id)
+    if (event) {
+      commit('SET_EVENT', event)
+    } else {
+      // storeに存在しない場合fetchする
+      console.log(id)
+      EventService.getEvent(id)
+        .then(response => {
+          commit('SET_EVENT', response.data)
+        })
+        .catch(error => {
+          console.log('There was an error:', error.response)
+        })
+    }
+  }
+}
+export const getters = {
+  getEventById: state => id => {
+    return state.events.find(event => event.id === id)
+  }
+}
+
+// export default {
+//   state: {
+//     events: [],
+//     eventTotal: 0,
+//     event: {}
+//   },
+//   mutations: {
+//     ADD_EVENT(state, event) {
+//       state.events.push(event)
+//     },
+//     SET_EVENTS(state, events) {
+//       state.events = events
+//     },
+//     SET_EVENT_TOTAL(state, eventTotal) {
+//       state.eventTotal = eventTotal
+//     },
+//     SET_EVENT(state, event) {
+//       state.event = event
+//     }
+//   },
+//   actions: {
+//     createEvent({ commit, /* dispatch */ rootState }, event) {
+//       console.log(`User creating Event is ${rootState.user.user.name}`)
+
+//       // 別モジュールのactionを実行したいとき
+//       // dispatchEvent('actionToCall')
+
+//       return EventService.postEvent(event)
+//         .then(() => {
+//           commit('ADD_EVENT', event.data)
+//         })
+//         .catch(() => {
+//           console.log('There was a problem creating your event')
+//         })
+//     },
+//     fetchEvents({ commit }, { perPage, page }) {
+//       EventService.getEvents(perPage, page)
+//         .then(response => {
+//           commit('SET_EVENTS', response.data)
+//           commit('SET_EVENT_TOTAL', parseInt(response.headers['x-total-count']))
+//         })
+//         .catch(error => {
+//           console.log('There was an error:', error.response)
+//         })
+//     },
+//     fetchEvent({ commit, rootGetters }, id) {
+//       let event = rootGetters.getEventById(id)
+//       if (event) {
+//         commit('SET_EVENT', event)
+//       } else {
+//         // storeに存在しない場合fetchする
+//         console.log(id)
+//         EventService.getEvent(id)
+//           .then(response => {
+//             commit('SET_EVENT', response.data)
+//           })
+//           .catch(error => {
+//             console.log('There was an error:', error.response)
+//           })
+//       }
+//     }
+//   },
+//   getters: {
+//     getEventById: state => id => {
+//       return state.events.find(event => event.id === id)
+//     }
+//   }
+// }
---
+++ src/views/EventList.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h1>Events for {{ user.user.name }}</h1>
+    <EventCard v-for="event in event.events" :key="event.id" :event="event" />
+    <template v-if="page != 1">
+      <router-link
+        :to="{ name: 'event-list', query: { page: page - 1 } }"
+        rel="prev"
+        >Prev Page</router-link
+      >
+      <template v-if="hasNextPage"> | </template>
+    </template>
+    <router-link
+      v-if="hasNextPage"
+      :to="{ name: 'event-list', query: { page: page + 1 } }"
+      >Next Page</router-link
+    >
+  </div>
+</template>
+<script>
+import { mapState } from 'vuex'
+import EventCard from '@/components/EventCard.vue'
+
+export default {
+  components: {
+    EventCard
+  },
+  created() {
+    this.perPage = 3
+    this.$store.dispatch('event/fetchEvents', {
+      perPage: this.perPage,
+      page: this.page
+    })
+  },
+  computed: {
+    page() {
+      return parseInt(this.$route.query.page) || 1
+    },
+    hasNextPage() {
+      return this.event.eventTotal > this.page * this.perPage
+    },
+    ...mapState(['event', 'eventTotal', 'user'])
+  }
+}
+</script>
